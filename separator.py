ebee080700d37e53028ad3cb5a687d3f2ab1275c
Lucas Walger <walger.profissional@gmail.com>
2023-11-11 17:37:45 -0300

    funcoes de setup de variaveis, constantes e operacoes esta pronta

--- separator.py
+++ separator.py
@@ -1,4 +1,6 @@
-divisorWords = (' e ',' ou ','==','!=','<','>','!','(',')',',','própega','ladi','com','di cima', 'di baixo') 
+from table import TableCoordinate
+divisorWords = (' e ',' ou ','==','!=','<','>','!','(',')',',','própega','ladi','com','di cima', 'di baixo')
+# append da palavra nao divisora entre palavras divisoras
 def tryAppendLastWord(text,word_start,word_end, separated_text):
     if word_start >= len(text):
         return
@@ -12,6 +14,7 @@ def tryAppendLastWord(text,word_start,word_end, separated_text):
             j-=1
         separated_text.append(text[start:word_end+j])
 
+#divide texto em uma lista de variáveis e operacoes
 def separator(text):
     separated_text = []
     txt_length = len(text)
@@ -20,12 +23,15 @@ def separator(text):
     i=0
     word_start = 0
     while i <txt_length:
+        # lida com quotations, nao lendo operacoes dentro de quotations
         if (text[i] == '"' or text[i] == "'") and (not in_quotation or qu_symbol==text[i]):
             in_quotation = not in_quotation
             qu_symbol = text[i]
         if not in_quotation:
+            #tenta encontrar palavras divisoras na substring
             for dW in divisorWords:
                 if i+len(dW)<=txt_length and text[i:i+len(dW)].lower() == dW:
+                    #se encontrou uma, primeiro adicione a palavra nao divisora anterior a palavra divisora
                     tryAppendLastWord(text,word_start,i,separated_text)
                     separated_text.append(dW)
                     i+=len(dW)
@@ -58,7 +64,69 @@ def removeUnecessaryParenthesis(sep_text):
          sep_text.pop()
          sep_text.pop(0)
 
+#faz setup de variaveis e constantes, apenas para statements where
+def setupVariablesAndConstants(sep_text, tables):
+    new_text = [] #variável que guardará nova forma do texto
+    for word in sep_text:
+        #passa palavras divisoras sem analisar
+        if word in divisorWords:
+            new_text.append(word)
+            continue
+        # encontra quotations e os tira, tornando apenas uma string comum
+        if (word[0]== '"' and word[-1]== '"') or (word[0]== "'" and word[-1]== "'"):
+            new_text.append(word[1:-1])
+            continue
+        #analisa se apenas há numeros e ou um ponto, e os transforma em ints e floats
+        isNumber,isFloat = IsNumeric(word)
+        if isNumber and isFloat:
+            new_text.append(float(word)) 
+            continue
+        elif isNumber:
+            new_text.append(int(word)) 
+            continue
+        #Procura por uma tabela.coluna no texto
+        new_text.append(IsTable(word,tables))
+    return new_text
 
+# retorna se string eh um numero ou um inteiro ou nenhum
+def IsNumeric(word):
+    isFloat = False
+    isNumber = True
+    for c in word:
+        if c=='.' and isFloat==False:
+            isFloat=True
+        elif c=='.':
+            isNumber=False
+            break
+        elif c<'0' or c>'9':
+            isNumber=False
+            break
+    return isNumber,isFloat
+
+# testa se palavra eh uma coluna de alguma das tabelas dads, e retorna um TableCoordinate caso sim, e uma Exception caso nao
+def IsTable(word,tables):
+    len_word = len(word)
+    dot_position = -1
+    for i in range(len_word):
+        if (word[i]=='.' and dot_position == -1 ):
+            dot_position=i
+        elif word[i]=='.':
+            raise Exception(word + "não é uma variável válida.")
+    if dot_position == -1:
+        raise Exception(word + "não é uma variável válida.")
+    tables_len = len(tables)
+    found_table = False
+    for j in range(tables_len):
+        if tables[j].name == word[:dot_position]:
+            column_id = tables[j].getColumn(word[dot_position+1:])
+            found_table=True
+            if column_id!=-1:
+                return TableCoordinate(j,column_id)
+            else:
+                raise Exception("Tabela " + tables[j].name + " não possui coluna " + word[dot_position+1:]+" .")
+            break
+    if not found_table:
+        raise Exception("Não há uma tabela " + word[:dot_position]+ " nesta query.")
 
 
 
